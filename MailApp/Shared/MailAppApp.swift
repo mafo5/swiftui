2812813ecd20421cd320e310c4896261abdcb79e
Mafo5 <mafo.work@googlemail.com>
2021-08-05 09:14:51 +0200

    create new app for mailapp

---
+++ MailApp/Shared/MailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailAppApp.swift
+//  Shared
+//
+//  Created by Ma Fo on 05.08.21.
+//
+
+import SwiftUI
+
+@main
+struct MailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
